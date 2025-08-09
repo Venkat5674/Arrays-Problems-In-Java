fe82c1bc715f3a78da9de294ed70acd0ad79fa07
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-09 23:09:24 +0530

    Create Swapping.java

---
+++ Swapping.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Swapping {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
