724b255882f69a14b2a0cdfdbecac4fe4f3bb3d9
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-11 21:19:39 +0530

    Create AverageofArray.java

---
+++ AverageofArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class AverageOfArray{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the size of the array : ");
+        int size = sc.nextInt();
+        int [] arr = new int[size];
+        
+        int sum = 0;
+        System.out.println("Enter the Elements of Array : ");
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i]; 
+        }
+        
+        double average = (double) sum / size;
+        
+        System.out.println("The average of Array is : " + average);
+    }
+}
