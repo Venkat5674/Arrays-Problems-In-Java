bcac5f87d9ba8df120b6c7f7dee0ad83d7779654
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-27 23:43:07 +0530

    Create ArrayTraversal.java

---
+++ AverageArray1/ArrayTraversal.java
@@ -0,0 +1,10 @@
+class ArrayTraversal {
+    public static void arrayTraversal(int[] arr) {
+        // Code here
+       for(int i=0; i<arr.length; i++){
+          System.out.print(arr[i]+ " ");
+       } 
+       //System.out.println();
+    }
+    
+}
