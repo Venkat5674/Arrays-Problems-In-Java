39848417f5e61e6a36c73853f8a3796e0705b7b6
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-27 23:34:20 +0530

    Add LengthArray class with arrayLength method
    Implement method to count the length of an array.

---
+++ AverageArray1/LengthArray.java
@@ -0,0 +1,10 @@
+class LengthArray {
+    public static int arrayLength(int[] arr) {
+        // code here
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            count++;
+        }
+        return count;
+    }
+}
