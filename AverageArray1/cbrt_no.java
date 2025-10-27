a58a0ee91a2c40a42589982c9b7e7929b2454bd7
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-10-28 00:28:51 +0530

    Add cbrt_no.java to compute cube roots of array elements

---
+++ AverageArray1/cbrt_no.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class cbrt_no {
+  
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number of elements in the array:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.printf("%.2f\n", Math.cbrt(arr[i]));
+        }
+    }
+}
